d2f5b7c7219be7c471ea832e5bac89dd90e42036
BrugerX <Benjaminmiradgurini@gmail.com>
2024-06-05 19:28:09 +0200

    SoupExtractor finished and basic tests

---
+++ LotData/ExtractorsAndTables.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import Browser as brws
+import re
+import utility.webscrapingUtil as wbsu
+
+class DownloadedData:
+
+    def __init(self,timestamp):
+        self.downloaded_timestamp = timestamp
+
+"""
+
+    Responsible for extracting data out of the website's soup.
+
+"""
+
+class SoupExtractor(DownloadedData):
+
+    def __init__(self,timestamp,soup):
+        DownloadedData.__init__(timestamp)
+        self.soup = soup
+
+        self.euroNumberPattern = "€([0-9])+(,([0-9])+)?"  # Pattern for € 8,000
+        self.expertEstimatePattern = re.compile(f"expertestimate{self.euroNumberPattern}-{self.euroNumberPattern}")
+
+
+    def getSpecs(self):
+        specs = self.soup.findAll("div", {"class": "be-lot-specifications u-m-t-sm-xl u-m-t-xxl-2"})
+        specifications = {}
+
+        # Iterate over all spec values and names and turn them into a dict.
+        for spec in specs[0].find_all("div", class_="be-lot-specification"):
+            name = spec.find("span", class_="be-lot-specification__name").get_text(strip=True)
+            value = spec.find("div", class_="be-lot-specification__value").get_text(strip=True)
+            specifications[name] = value
+
+        return specifications
+
+    def getSoup(self):
+        return self.soup
+
+
+    """
+    
+    @input A BS4 soup of the website that has accounted for whether the auction was closed or not at the time of scraping
+    @return Expert's estimate of an auction, with (m1,m2) where m1<=m2.
+    """
+
+    def getExpertEstimates(self):
+        spans = self.soup.find_all("span")
+
+        #Find the span with the expert estimate text
+        for idx,span in enumerate(spans):
+            spanParentText = span.find_parent().text
+            sanitizedParentText = "".join(spanParentText.split()).lower()  # Lowercase without spaces between
+
+
+            if (re.match(self.expertEstimatePattern, sanitizedParentText) is not None):
+                #We have now found the proper span
+                return self.extractExpertEstimateFromText(sanitizedParentText)
+
+        raise RuntimeError(f"Didn't find expert estimate in the list of spans, got to index {idx} \n With spans: {spans}")
+
+
+    """
+
+    @:arg expertEstSpan The sanitized text of the span node and its parent, such that their combined RAW text is of the form:
+        "Expert Estimate € 8,000 - € 9,000" - where the second number always has to be larger than the first.
+
+        The sanitized text must be all lowercase with no white spaces, such that
+        "Expert Estimate € 8,000 - € 9,000" -> "expertestimate€8,000-€9,000"
+
+
+    @return The expert's estimates as ints, where return[0] < return[1] - example [8000,9000]
+    """
+
+    def extractExpertEstimateFromText(self,expertEstSpanText):
+        estMinMax = wbsu.multipleReplaceReGeX({"expertestimate": "", "€": "", ",": ""}, expertEstSpanText).split(
+            "-")  # Remove unneccessary chars and split it on the "-"
+        estMinMax = [int(est) for est in estMinMax]
+
+        if (estMinMax[0] < estMinMax[1]):
+            return tuple(estMinMax)  # We turn it into a tuple, so we don't accidentally mutate it later
+        else:
+            raise RuntimeError(
+                f"Our scraping method for expert's estimates didn't work.\n For the following test: {expertEstSpanText} we got the following estimate min: {estMinMax[0]} and max {estMinMax[1]}")
+
+
+
---
+++ Tests/ExtractorsAndTables.py
@@ -0,0 +1,48 @@
+import unittest
+import LotData.ExtractorsAndTables as EnT
+import Browser
+
+class SoupExtractorTests(unittest.TestCase):
+
+
+
+    """
+    The extractor can get specs from two different categories
+    """
+    def test_soup_gets_specs_diamonds_watches(self):
+        closed_LID_diamonds = "84559939"
+        closed_LID_watches = "84654169"
+
+        soup_diamonds = Browser.SeleniumBrowser.getClosedAuctionSoup(closed_LID_diamonds)
+        soup_extractor_diamonds = EnT.SoupExtractor("",soup_diamonds)
+
+        realSpecsDiamonds = {'Diamond type': 'Natural', 'Lab report number': '532234282', 'Number of diamonds': '1', 'Total carat weight': '0.48', 'Cut grade': 'Excellent', 'Diamond Clarity Grade': 'SI2', 'Fluorescence': 'Very slight fluorescence', 'Symmetry': 'Very good', 'Polish': 'Very good', 'Laboratory Report': 'International Gemological Institute (IGI)', 'Sealed by laboratory': 'Yes', 'Laser Engraved': 'No', 'Shape': 'Round', 'Cutting style': 'Brilliant cut'}
+        self.assertEqual(realSpecsDiamonds,soup_extractor_diamonds.getSpecs())  # add assertion here
+
+        soup_watches = Browser.SeleniumBrowser.getClosedAuctionSoup(closed_LID_watches)
+        soup_extractor_watches = EnT.SoupExtractor("",soup_watches)
+        real_specs_watches = {'Brand': 'TAG Heuer', 'Case material': 'Steel', 'Condition': 'Worn & in very good condition', 'Diameter/ Width Case': '45 mm', 'Era': 'After 2000', 'Extras': 'Box, Documents, Warranty', 'Gender': 'Men', 'Length watch band': '165 mm', 'Model': 'Connected', 'Movement': 'Quartz', 'Period': '2011-present', 'Reference Number': 'SBG8A10', 'Shipped Insured': 'Yes'}
+        self.assertEqual(real_specs_watches,soup_extractor_watches.getSpecs())
+
+    def test_soup_gets_experts_estimate_diamonds_watches(self):
+        closed_LID_diamonds = "84559939"
+        closed_LID_watches = "84654169"
+
+        soup_diamonds = Browser.SeleniumBrowser.getClosedAuctionSoup(closed_LID_diamonds)
+        soup_extractor_diamonds = EnT.SoupExtractor("",soup_diamonds)
+
+        soup_watches = Browser.SeleniumBrowser.getClosedAuctionSoup(closed_LID_watches)
+        soup_extractor_watches = EnT.SoupExtractor("",soup_watches)
+
+        real_experts_estimates_diamonds = (1300,1600)
+        real_experts_estimates_watches = (1100,1300)
+
+        self.assertEqual(real_experts_estimates_watches,soup_extractor_watches.getExpertEstimates())
+        self.assertEqual(real_experts_estimates_diamonds,soup_extractor_diamonds.getExpertEstimates())
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
